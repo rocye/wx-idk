70b81adba6124f7b8420dd1695c92db0758961c8
Rocye <hirocye@163.com>
2017-10-13 17:17:47 +0800

    评论数据管理

---
+++ src/main/java/org/wx/sdk/material/request/CommentReplyAddRequest.java
@@ -0,0 +1,124 @@
+package org.wx.sdk.material.request;
+
+import org.wx.sdk.base.Request;
+import org.wx.sdk.material.response.CommentRespone;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 回复评论请求对象
+ * @author Rocye
+ * @version 2017.10.13
+ */
+public class CommentReplyAddRequest implements Request<CommentRespone> {
+
+	/** 微信公众平台唯一接口凭证 */
+    private String accessToken;
+    /** 请求参数的Map */
+    private Map<String, Object> wxHashMap = new HashMap<String, Object>();
+    /** 群发返回的id */
+    private Long msg_data_id;
+    /** 多图文时，用来指定第几篇图文，从0开始，不带默认操作该msg_data_id的第一篇图文 */
+    private Integer index;
+    /** 用户评论id */
+    private Integer user_comment_id;
+    /** 回复内容 */
+    private String content;
+
+    /**
+     * 构造器
+     * @param msgDataId  群发返回的id
+     * @param index  多图文时，用来指定第几篇图文
+     * @param userCommentId  用户评论id
+     * @param content  回复内容
+     */
+    public CommentReplyAddRequest(Long msgDataId, Integer index, Integer userCommentId, String content) {
+        this.msg_data_id = msgDataId;
+        this.index = index;
+        this.user_comment_id = userCommentId;
+        this.content = content;
+    }
+
+    /**
+     * 获取接口请求地址
+     */
+    public String getApiUrl(){
+        String url = "https://api.weixin.qq.com/cgi-bin/comment/reply/add?access_token="+ this.accessToken;
+        return url;
+    }
+
+    /**
+     * 获取返回对象类
+     */
+    public Class<CommentRespone> getResponseClass(){
+        return CommentRespone.class;
+    }
+    
+    /**
+     * 获取请求参数的HashMap
+     */
+    public Map<String, Object> getWxHashMap(){
+        wxHashMap.put("msg_data_id", this.msg_data_id);
+        wxHashMap.put("index", this.index);
+        wxHashMap.put("user_comment_id", this.user_comment_id);
+        wxHashMap.put("content", this.content);
+        return wxHashMap;
+    }
+    
+    /**
+     * 获取请求是否是Https
+     */
+    public boolean getUseHttps(){
+        return true;
+    }
+    
+    /**
+     * 请求类型：1-普通Get 2-下载GET 3-普通POST 4-下载POST 5-无参上传   6-有参上传
+     */
+    public int getReqType(){
+        return 3;
+    }
+    
+    /**
+     * 请求参数格式(kv,json,xml)
+     */
+    public String getParamFormat(){
+        return "json";
+    }
+    
+    /**
+     * 设置AccessToken
+     */
+    public void setAccessToken(String accessToken){
+        this.accessToken = accessToken;
+    }
+
+    public Long getMsg_data_id() {
+		return msg_data_id;
+	}
+	public void setMsg_data_id(Long msg_data_id) {
+		this.msg_data_id = msg_data_id;
+	}
+
+	public Integer getIndex() {
+        return index;
+    }
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Integer getUser_comment_id() {
+        return user_comment_id;
+    }
+    public void setUser_comment_id(Integer user_comment_id) {
+        this.user_comment_id = user_comment_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
--- src/test/java/org/wx/sdk/material/CommentReqTest.java
+++ src/test/java/org/wx/sdk/material/CommentReqTest.java
@@ -3,9 +3,7 @@ package org.wx.sdk.material;
 import org.junit.Test;
 import org.wx.sdk.WxClient;
 import org.wx.sdk.base.Const;
-import org.wx.sdk.material.request.CommentCloseRequest;
-import org.wx.sdk.material.request.CommentListRequest;
-import org.wx.sdk.material.request.CommentOpenRequest;
+import org.wx.sdk.material.request.*;
 import org.wx.sdk.material.response.CommentListRespone;
 import org.wx.sdk.material.response.CommentRespone;
 import org.wx.sdk.token.RedisAccessToken;
@@ -42,6 +40,50 @@ public class CommentReqTest {
         WxClient wxClient = new WxClient(new RedisAccessToken(Const.APPID, Const.APPSERCT));
         CommentListRequest commentListReq = new CommentListRequest(1L, 0, 1, 10, 0);
         CommentListRespone commentRes  = wxClient.excute(commentListReq);
+        System.out.println(commentRes.getBody());
+    }
+
+    /**
+     * 测试将评论标记精选
+     */
+    @Test
+    public void testCommentMarkelectReq() {
+        WxClient wxClient = new WxClient(new RedisAccessToken(Const.APPID, Const.APPSERCT));
+        CommentMarkelectRequest commentMarkelectReq = new CommentMarkelectRequest(1L, 0, 1);
+        CommentRespone commentRes  = wxClient.excute(commentMarkelectReq);
+        System.out.println(commentRes.getBody());
+    }
+
+    /**
+     * 测试删除评论
+     */
+    @Test
+    public void testCommentDeleteReq() {
+        WxClient wxClient = new WxClient(new RedisAccessToken(Const.APPID, Const.APPSERCT));
+        CommentDeleteRequest commentDeleteReq = new CommentDeleteRequest(1L, 0, 1);
+        CommentRespone commentRes  = wxClient.excute(commentDeleteReq);
+        System.out.println(commentRes.getBody());
+    }
+
+    /**
+     * 测试回复评论
+     */
+    @Test
+    public void testCommentReplyAddReq() {
+        WxClient wxClient = new WxClient(new RedisAccessToken(Const.APPID, Const.APPSERCT));
+        CommentReplyAddRequest commentReplyAddReq = new CommentReplyAddRequest(1L, 0, 1, "点赞点赞!");
+        CommentRespone commentRes  = wxClient.excute(commentReplyAddReq);
+        System.out.println(commentRes.getBody());
+    }
+
+    /**
+     * 测试删除回复
+     */
+    @Test
+    public void testCommentReplyDeleteReq() {
+        WxClient wxClient = new WxClient(new RedisAccessToken(Const.APPID, Const.APPSERCT));
+        CommentReplyDeleteRequest commentReplyDeleteReq = new CommentReplyDeleteRequest(1L, 0, 1);
+        CommentRespone commentRes  = wxClient.excute(commentReplyDeleteReq);
         System.out.println(commentRes.getBody());
     }
     
